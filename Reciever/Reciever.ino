4a9ec1a09fcff1b7625c629a84dab7e756b49246
Omer Re <39154767+omer-re@users.noreply.github.com>
2021-10-02 16:01:20 +0300

    Update Reciever.ino

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -4,13 +4,12 @@
 
 #include <IRremote.h>
 #include <Arduino.h>
-
+#include <FastLED.h>
 
 #define RECV_PIN 5
-#define GND_PIN 6
-#define VCC_PIN 7
 
-#define SIG_LED 1 //NOTE: this is just a regular indication led- has nothing to the with IR nor with the actual operation
+#define SIG_LED 4 //NOTE: this is just a regular indication led- has nothing to the with IR nor with the actual operation
+#define PENDING_LED 6
 #define POWER_LED 0 //NOTE: this is just a regular indication led- has nothing to the with IR nor with the actual operation
 
 IRrecv irrecv(RECV_PIN);
@@ -26,20 +25,25 @@ void flash_flash() {
     }
 }
 
+
 String sig_name;
 void setup() {
     Serial.begin(9600);
     irrecv.enableIRIn(); // Start the receiver
-    pinMode(GND_PIN, LOW);
-    pinMode(VCC_PIN, HIGH);
-    Serial.print("RAW_BUFFER_LENGTH\t");Serial.println(RAW_BUFFER_LENGTH);
 
+    pinMode(SIG_LED, OUTPUT);
+    pinMode(PENDING_LED, OUTPUT);
+
+    Serial.print("RAW_BUFFER_LENGTH\t");Serial.println(RAW_BUFFER_LENGTH);
+    flash_flash();
     Serial.println("READY TO GO");
 }
 
 void loop() {
     Serial.println("Type signal name: "); 
-    while (Serial.available() == 0){ }  
+    while (Serial.available() == 0){
+            EVERY_N_MILLISECONDS( 800 ) {digitalWrite(PENDING_LED, !digitalRead(PENDING_LED));}
+      }
     sig_name=Serial.readString();
     Serial.println(sig_name);  
     Serial.println("Now record remote operation: "); 
@@ -48,12 +52,14 @@ void loop() {
     while (irrecv.available() == 0){ }  
 
     if (irrecv.decode()) {
-        Serial.print("Receiving signal: \n");
+        Serial.print("Receiving signal:");
         irrecv.compensateAndPrintIRResultAsCArray(&Serial, sig_name);// prints data about the signal
         delay(1000);
         irrecv.resume(); // Receive the next value
         flash_flash();
-        Serial.print("Done \n");
+ 
+        Serial.print("Done, next\n");
+        
 
     }
 
